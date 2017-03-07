23fc1e5dbeea8e3127f8523226b1dd534944460d
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-03-07 15:53:15 +0100

    Some assembly logic.

---
+++ src/Graphics/OBJ/Assembly.hs
@@ -0,0 +1,93 @@
+{-# LANGUAGE MultiWayIf #-}
+module Graphics.OBJ.Assembly
+    ( basicAssembly
+    ) where
+
+import           Data.List           (foldl')
+import           Data.Vector         (Vector)
+import qualified Data.Vector         as Vector
+import           Graphics.GL         (GLfloat)
+import           Graphics.OBJ.Parser (Elem (..), Face (..), Part (..), fromFile)
+import           Linear              (V2, V3)
+
+data Assembly
+    = VAssembly !(Vector (V3 GLfloat)) !(Vector Face)
+    | VNAssembly !(Vector (V3 GLfloat)) !(Vector (V3 GLfloat)) !(Vector Face)
+    | VTNAssembly !(Vector (V3 GLfloat)) !(Vector (V3 GLfloat)) !(Vector (V2 GLfloat)) !(Vector Face)
+    | BrokenAssembly
+    deriving Show
+
+-- | From the parsed file parts, make a basic assembly to data structures from
+-- where real vertex and index data can be created. The basic assembly also
+-- performs a few sanity checks.
+basicAssembly :: [Part] -> Assembly
+basicAssembly xs =
+    let (vertices, normals, texCoords, faces) = partition xs
+    in
+        if  | faceType v faces &&
+                Vector.null normals &&
+                Vector.null texCoords ->
+                    VAssembly vertices faces
+            | faceType vn faces &&
+                not (Vector.null normals) &&
+                Vector.null texCoords  ->
+                VNAssembly vertices normals faces
+            | faceType vtn faces &&
+                not (Vector.null normals) &&
+                not (Vector.null texCoords) ->
+                VTNAssembly vertices normals texCoords faces
+            | otherwise -> BrokenAssembly
+
+partition :: [Part] -> ( Vector (V3 GLfloat), Vector (V3 GLfloat)
+                       , Vector (V2 GLfloat), Vector Face )
+partition xs = ( filterPart filterVertex xs, filterPart filterNormal xs
+               , filterPart filterTexCoord xs, filterPart filterFace xs )
+
+filterPart :: (a -> Maybe b) -> [a] -> Vector b
+filterPart f =
+    Vector.fromList . reverse . foldl' (\acc x -> maybe acc (\x' -> x':acc) (f x)) []
+
+filterVertex :: Part -> Maybe (V3 GLfloat)
+filterVertex part =
+    case part of
+        Vertex ve -> Just ve
+        _         -> Nothing
+
+filterNormal :: Part -> Maybe (V3 GLfloat)
+filterNormal part =
+    case part of
+        Normal n -> Just n
+        _        -> Nothing
+
+filterTexCoord :: Part -> Maybe (V2 GLfloat)
+filterTexCoord part =
+    case part of
+        TexCoord t -> Just t
+        _          -> Nothing
+
+filterFace :: Part -> Maybe Face
+filterFace part =
+    case part of
+        Face f -> Just f
+        _      -> Nothing
+
+faceType :: (Face -> Bool) -> Vector Face -> Bool
+faceType g = Vector.foldl' (\acc face -> acc && g face) True
+
+v :: Face -> Bool
+v face =
+    case face of
+        Triangle (V _) (V _) (V _ ) -> True
+        _                           -> False
+
+vn :: Face -> Bool
+vn face =
+    case face of
+        Triangle (VN _ _) (VN _ _) (VN _ _) -> True
+        _                                   -> False
+
+vtn :: Face -> Bool
+vtn face =
+    case face of
+        Triangle (VTN _ _ _) (VTN _ _ _) (VTN _ _ _) -> True
+        _                                            -> False
